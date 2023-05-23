5b2a59be3c3add9b63f9b73b5679d485a3950ccc
crow-kuma <zoologico.kc@gmail.com>
2023-05-23 19:06:53 +0900

    MyBatis trial failed

---
+++ mybatis-sample/src/main/java/jp/co/trainocamp/demo/mybatis/entity/MybatisUser.java
@@ -0,0 +1,10 @@
+package jp.co.trainocamp.demo.mybatis.entity;
+
+import lombok.Data;
+
+@Data
+public class MybatisUser {
+	private int userId;
+	private String name;
+	private int age;
+}
---
+++ mybatis-sample/src/main/java/jp/co/trainocamp/demo/mybatis/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package jp.co.trainocamp.demo.mybatis.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import jp.co.trainocamp.demo.mybatis.entity.MybatisUser;
+
+@Mapper
+public interface UserMapper {
+	List<MybatisUser> selectAll();
+}
