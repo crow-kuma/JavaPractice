09b0f3844e6a0b427e6cee8231e939672c1da64e
crow-kuma <zoologico.kc@gmail.com>
2023-05-26 13:48:35 +0900

    tasklistmbg first commit

---
+++ tasklistmbg/src/main/java/jp/gihyo/projavambg/tasklistmbg/TasklistmbgApplication.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projavambg.tasklistmbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistmbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistmbgApplication.class, args);
+	}
+
+}
---
+++ tasklistmbg/src/test/java/jp/gihyo/projavambg/tasklistmbg/TasklistmbgApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.projavambg.tasklistmbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistmbgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
